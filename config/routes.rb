03774b685ab4358352681a77be6940452d896171
Jason <jasylwong@gmail.com>
2020-07-16 12:53:14 +0100

    add Comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index', as: 'home'
-  resources :posts
+
   get 'about' => 'pages#about' 
+
+  resources :posts do
+    resources :comments
+  end
 end
