c27b4def217eb83ad4ef84b7b4fd502d3182cace
Jason <jasylwong@gmail.com>
2020-07-16 11:01:11 +0100

    add post form and show all in post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,5 +12,17 @@ class PostsController < ApplicationController
 
   def create
     # render plain: params[:post].inspect
+    # render plain: post_params.inspect
+    @post = Post.new(post_params)
+    @post.save
+    puts(params)
+    redirect_to @post
   end
+
+  private 
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
